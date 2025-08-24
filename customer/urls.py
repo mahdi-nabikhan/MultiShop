7af83a6b558ff0a93d7c49df19db8c7bb4dcb851
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2025-08-24 17:54:31 +0330

    change in projects  * change in order customer app   + add customer register template view   + add more detail at costomer_register.html

--- customer/urls.py
+++ customer/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns=[
-    path('api/v1/',include('customer.api.v1.urls'))
+    path('api/v1/',include('customer.api.v1.urls')),
+    path('regsiter/',CostumerRegisterTemplateView.as_view(),name='register'),
 
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+class CostumerRegisterTemplateView(TemplateView):
+    template_name = 'accounts/costomer_register.html'
