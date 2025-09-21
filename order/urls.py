e32fa9ac704c1c8d9568947b2ec2c74478b6af92
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2025-09-21 15:20:22 +0330

    Changes in project   Update in order app:     - Added OrderDetailView for rendering The cart.html     - Fixing cart.html for render (extended form base.html)

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
-
+from .views import *
 urlpatterns = [
-    path('api/v1/', include('order.api.v1.urls'))
+    path('api/v1/', include('order.api.v1.urls')),
+    path('orders/',OrderDetailView.as_view(),name='order-detail')
+
 
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+
+class OrderDetailView(TemplateView):
+    template_name = 'orders/cart.html'
