b6fc44e9bcf9f0483edcfff226f97d38240bd596
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2025-11-15 14:08:03 +0330

    Registered API routes by including `api/urls.py` in main app URL configuration

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path,include
 from .views import *
 
+
 urlpatterns = [
+    path('api/v1/', include('website.api.v1.urls'),name='webste_api_v1'),
     path('shop/list/', AllShopView.as_view(), name='shop-list'),
     path('product/list/<int:pk>/', StoreProductsViews.as_view(), name='product-list'),
     path('shop/detail/<int:pk>/',ShopDetailView.as_view(), name='shop-detail'),
