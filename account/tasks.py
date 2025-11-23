067f70e5b66e8e4335f098703a4ba478d160b9fa
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2025-11-23 18:24:27 +0330

    add send_welcome_email_task in task.py for sending welcome email with n8n

--- account/tasks.py
+++ account/tasks.py
@@ -1,5 +1,6 @@
 from celery import shared_task
 from django.core.mail import send_mail
+import requests
 
 @shared_task
 def send_reset_code_email(email, code):
@@ -10,3 +11,23 @@ def send_reset_code_email(email, code):
         recipient_list=[email],
         fail_silently=False
     )
+
+
+@shared_task(bind=True, max_retries=3, default_retry_delay=60)
+def send_welcome_email_task(self, user_email, user_name):
+    """
+    ارسال ایمیل خوش آمدگویی با n8n Webhook
+    """
+    url = "http://n8n:5678/webhook/send-email"  # داخل Docker از hostname کانتینر استفاده می‌کنیم
+    data = {
+        "email": user_email,
+        "subject": "خوش آمدید به Multi Shop!",
+        "message": f"سلام {user_name} عزیز، خوش آمدید به Multi Shop 😊"
+    }
+    try:
+        res = requests.post(url, json=data, timeout=10)
+        res.raise_for_status()
+        return res.status_code
+    except requests.RequestException as exc:
+        # اگر خطایی بود، تسک رو دوباره retry کن
+        raise self.retry(exc=exc)
