668d466211c8e345af12596a86d06d0cbb037a29
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-05 15:11:50 +0330

    feat(backend & docker compose):add postgresql to docker compose of project project and load on settings.py

--- src/backend/Multi_Shop/settings.py
+++ src/backend/Multi_Shop/settings.py
@@ -87,13 +87,22 @@ WSGI_APPLICATION = 'Multi_Shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "multi_shop_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
