56bf5d5f283a288812fdcd1e61af26e1a31f6d15
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-09 12:03:31 +0330

    feat(frontend):user OrderDetail component on order app

---
+++ src/frontend/app/order/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import OrderDetail from '@/components/order/Order/OrderDetail'
+export default function order(){
+  return (
+    <div>
+      <OrderDetail/>
+    </div>
+  )
+}
