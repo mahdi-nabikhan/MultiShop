da8a0fb67edb9d62b939e99ae4cff8937d8030c8
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-11 15:04:21 +0330

    feat(frontend):add file for profuct list showing remove(backend): remove redirect url form jwt login view

---
+++ src/frontend/components/shop/ShopList/ShopList.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import BACKEND_URLS from '@/utils'
+function ShopList() {
+  return (
+    <div>ShopList</div>
+  )
+}
+
+export default ShopList
--- src/frontend/utils.tsx
+++ src/frontend/utils.tsx
@@ -1,1 +1,2 @@
 const BACKEND_URLS : string = 'localhost:8000/'
+export default BACKEND_URLS
