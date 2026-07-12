8142a8e1d8d608898f6972bbdedbdf4c750b0550
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-12 18:35:22 +0330

    refactor(frontend): add new method for getting data from api

--- src/frontend/components/shop/ProductList/ProductList.tsx
+++ src/frontend/components/shop/ProductList/ProductList.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import ProductCard from "../ProductCard/ProductCard";
 import "./ProductList.css";
 import BACKEND_URLS from "@/utils";
+import { cookies } from "next/headers";
 
 interface ProductListProps {
   shopId: string;
@@ -17,15 +18,27 @@ export interface Product {
   image: string | null;
   category: number;
   store: number;
-  
+
 }
 
 export default async function ProductList({ shopId }: ProductListProps) {
 
+  const cookieStore = await cookies();
+  const token = cookieStore.get("access")?.value;
+
+  const headers: Record<string, string> = {};
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
 
   const { data: products } = await axios.get<Product[]>(
-    `${BACKEND_URLS}website/api/v1/product/list/${shopId}`
+    `${BACKEND_URLS}website/api/v1/product/list/${shopId}`,
+    {
+      headers,
+    }
   );
+
   return (
     <section className="product-list container">
       <div className="product-list-header">
