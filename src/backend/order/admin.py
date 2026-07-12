683a1020fdeffa1c28ce5725cfbb3fbe1b5e7b73
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-12 09:30:15 +0330

    refactor(backend): add new admin for models of order app

--- src/backend/order/admin.py
+++ src/backend/order/admin.py
@@ -1,7 +1,147 @@
-from django.contrib import admin
-from .models import Bill, OrderItem, Order
 
-# Register your models here.
-admin.site.register(Bill)
-admin.site.register(OrderItem)
-admin.site.register(Order)
+from django.contrib import admin
+
+from .models import Order, OrderItem, Bill
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    autocomplete_fields = ("product",)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "customer",
+        "status",
+        "created",
+    )
+
+    list_filter = (
+        "status",
+        "created",
+    )
+
+    search_fields = (
+        "customer__user__email",
+        "customer__username",
+    )
+
+    ordering = ("-created",)
+
+    autocomplete_fields = (
+        "customer",
+    )
+
+    readonly_fields = (
+        "created",
+    )
+
+    list_editable = (
+        "status",
+    )
+
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "product",
+        "quantity",
+        "total",
+        "status",
+        "created",
+    )
+
+    list_filter = (
+        "status",
+        "created",
+        "product",
+    )
+
+    search_fields = (
+        "order__customer__user__email",
+        "product__name",
+    )
+
+    ordering = ("-created",)
+
+    autocomplete_fields = (
+        "order",
+        "product",
+    )
+
+    readonly_fields = (
+        "total",
+        "created",
+    )
+
+    list_editable = (
+        "status",
+    )
+
+    fieldsets = (
+        (
+            "Order Item",
+            {
+                "fields": (
+                    "order",
+                    "product",
+                    "quantity",
+                    "status",
+                )
+            },
+        ),
+        (
+            "Pricing",
+            {
+                "fields": (
+                    "total",
+                    "created",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "cart",
+        "address",
+        "status",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "created_at",
+    )
+
+    search_fields = (
+        "cart__customer__user__email",
+        "address__street",
+        "address__city",
+    )
+
+    ordering = ("-created_at",)
+
+    autocomplete_fields = (
+        "cart",
+        "address",
+    )
+
+    readonly_fields = (
+        "created_at",
+    )
+
+    list_editable = (
+        "status",
+    )
+
