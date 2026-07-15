ad77f5fe9bfd383941a4a46b22f7f3483b78758a
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-15 10:30:38 +0330

    feat(Frontend):add chart for showing product charts

--- src/frontend/app/shop-admin-panel/page.tsx
+++ src/frontend/app/shop-admin-panel/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import ProductChart from '@/components/admin panel/Charts/Charts'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div><ProductChart/></div>
   )
 }
