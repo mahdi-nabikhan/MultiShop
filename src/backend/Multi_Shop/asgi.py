189ee3dfb84d2c67ccb73d11dcbd4de17862fc53
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-15 12:51:14 +0330

    feat(backend):add cunsomer and routing an user routing inside of asgi

--- src/backend/Multi_Shop/asgi.py
+++ src/backend/Multi_Shop/asgi.py
@@ -1,11 +1,15 @@
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-
+import dashboard.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Multi_Shop.settings')
 
 application = ProtocolTypeRouter({
-    'http':get_asgi_application(),
+    "http":get_asgi_application(),
+    "websocket":AuthMiddlewareStack(
+        URLRouter(dashboard.routing.websocket_urlpatterns)
+    )
     
 })
