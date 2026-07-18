e382fd8440cd742b686a11882bf255bd3168cb3d
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-18 16:29:06 +0330

    feat(backend):add serializer  CartItemSerializer for session cart create

--- src/backend/order/api/v1/serializer.py
+++ src/backend/order/api/v1/serializer.py
@@ -186,3 +186,11 @@ class BillSerilizers(serializers.ModelSerializer):
         order=Order.objects.get(pk=self.context.get('pk'))
         validated_data['cart']=order
         return Bill.objects.create(**validated_data)
+    
+    
+
+
+class CartItemSerializer(serializers.Serializer):
+    product = ProductSerializer()
+    quantity = serializers.IntegerField()
+    total_price = serializers.DecimalField(...)
