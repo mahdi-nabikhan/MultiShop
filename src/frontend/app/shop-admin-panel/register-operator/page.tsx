d27e89246d253fc6b3e175b6b5643e8b7b355aa8
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-18 12:53:47 +0330

    feat(frontend): add OperatorRegisterForm component and add new route in admin panel for register a operator

---
+++ src/frontend/app/shop-admin-panel/register-operator/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import OperatorRegisterForm from '@/components/admin panel/OperatorRegisterForm/OperatorRegisterForm'
+export default function page() {
+  return (
+    <div>
+        <OperatorRegisterForm/>
+    </div>
+  )
+}
