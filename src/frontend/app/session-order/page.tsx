7b1869238b91ece4f072cac3fe29b55386955e25
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-07-28 18:48:03 +0330

    feat(frontend): add new app route and component named sessionOrder for listing Orderitem on sessions

---
+++ src/frontend/app/session-order/page.tsx
@@ -0,0 +1,12 @@
+import SessionOrder from '@/components/order/SessionOrder/SessionOrder'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <SessionOrder/>
+    </>
+  )
+}
+
+export default page
