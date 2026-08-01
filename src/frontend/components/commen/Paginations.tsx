499a96b1eaef19d05221a056bb1a7cc4f1adfbf5
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-08-01 18:10:55 +0330

    feat(frontend):add new pagination component for paginating bettwen pages

---
+++ src/frontend/components/commen/Paginations.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import "./Pagination.css";
+
+interface PaginationProps{
+
+    next:string | null;
+
+    previous:string | null;
+
+    loading?:boolean;
+
+    onNext:()=>void;
+
+    onPrevious:()=>void;
+
+}
+
+export default function Pagination({
+
+    next,
+
+    previous,
+
+    loading=false,
+
+    onNext,
+
+    onPrevious
+
+}:PaginationProps){
+
+    return(
+
+        <div className="pagination-container">
+
+            <button
+
+                className="pagination-btn"
+
+                disabled={!previous || loading}
+
+                onClick={onPrevious}
+
+            >
+
+                ← Previous
+
+            </button>
+
+
+            <button
+
+                className="pagination-btn"
+
+                disabled={!next || loading}
+
+                onClick={onNext}
+
+            >
+
+                Next →
+
+            </button>
+
+        </div>
+
+    );
+
+}
