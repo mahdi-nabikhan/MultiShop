17ea084f2baae8b6f4eaed33e1a213de437fa076
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-08-01 18:42:27 +0330

    bug fix for pages of shoplist

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,82 +1,44 @@
-import Footer from '@/components/shop/Footer/Footer'
-import Navbar from '@/components/shop/Navbar/Navbar'
-import Topbar from '@/components/shop/Topbar/Topbar'
-import SectionHeader from '@/components/shop/SectionHeader/SectionHeader'
-import ShopList from '@/components/shop/ShopList/ShopList'
-import RandomProducts from '@/components/shop/RandomProduct/RandomProduct'
-
+import Footer from "@/components/shop/Footer/Footer";
+import Navbar from "@/components/shop/Navbar/Navbar";
+import Topbar from "@/components/shop/Topbar/Topbar";
+import SectionHeader from "@/components/shop/SectionHeader/SectionHeader";
+import ShopList from "@/components/shop/ShopList/ShopList";
+import RandomProducts from "@/components/shop/RandomProduct/RandomProduct";
 
 interface Props {
-
-  searchParams: {
-
+  searchParams: Promise<{
     page?: string;
-
-  };
-
+  }>;
 }
 
+export default async function Page({ searchParams }: Props) {
+  const params = await searchParams;
 
-
-export default async function Page({
-
-  searchParams
-
-}: Props) {
-
-
-  const page = searchParams.page ?? "1";
-
+  const page = params.page ?? "1";
 
   return (
-
     <div>
+      <Navbar />
 
-      <Navbar/>
-
-      <Topbar/>
-
+      <Topbar />
 
       <div>
-
-
         <SectionHeader
-
           title="Shops"
-
           description="List Of All Shops On This Site"
-
         />
 
-
-        <ShopList
-
-          page={page}
-
-        />
-
-
+        <ShopList page={page} />
 
         <SectionHeader
-
           title="Products"
-
-          description="List Of Products on this On This Site"
-
+          description="List Of Products On This Site"
         />
 
-
-        <RandomProducts/>
-
-
+        <RandomProducts />
       </div>
 
-
-      <Footer/>
-
-
+      <Footer />
     </div>
-
   );
-
 }
