6fde319974cd6f6d246b2ac85019865d9d869821
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-08-06 11:39:33 +0330

    feat(frontend):Add new app route for showing list of order items and add compnen named Customerorderlist

---
+++ src/frontend/app/customer-panel/orderitem/[orderitemId]/page.tsx
@@ -0,0 +1,30 @@
+import CustomerOrderItemList from "../../../../components/customer panel/CustomerOrderItemList/CustomerOrderItemList";
+
+
+interface Props {
+
+    params: Promise<{
+        id:string;
+    }>;
+
+}
+
+
+
+export default async function Page({params}:Props){
+
+
+    const {id} = await params;
+
+
+
+    return (
+
+        <CustomerOrderItemList
+            orderId={Number(id)}
+        />
+
+    );
+
+
+}
