a06b811732b2af188b59be7a73616bd12f9420cc
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-08-09 10:12:59 +0330

    feat(fronetnd):add new component for showing deatil of customer and add new app route named profile in customer-panel

---
+++ src/frontend/app/customer-panel/profile/page.tsx
@@ -0,0 +1,13 @@
+import CustomerProfile from '@/components/customer-panel/CustomerProfile/CustomerProfile'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+    <CustomerProfile/>
+    
+    </>
+  )
+}
+
+export default page
