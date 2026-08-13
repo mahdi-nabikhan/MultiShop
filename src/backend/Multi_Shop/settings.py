4dc589b3174955742ae422ec07e9571a04854d00
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-08-13 11:42:34 +0330

    change in settings.py and add staticfiles

--- src/backend/Multi_Shop/settings.py
+++ src/backend/Multi_Shop/settings.py
@@ -151,10 +151,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',  
+    BASE_DIR / 'staticfiles',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
